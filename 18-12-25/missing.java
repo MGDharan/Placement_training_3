4522038d84b49cb206f99522208469ebf2f1f812
Giridharan <151344363+MGDharan@users.noreply.github.com>
2025-12-19 02:38:36 -0800

    Create missing.java

---
+++ 18-12-25/missing.java
@@ -0,0 +1,12 @@
+public class MissingNumber {
+    public static void main(String[] args) {
+        int[] nums = {3, 0, 1};
+        int n = nums.length;
+        int sum = n * (n + 1) / 2;
+
+        for (int num : nums)
+            sum -= num;
+
+        System.out.println(sum);
+    }
+}
