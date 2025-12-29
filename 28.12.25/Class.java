c06eaada65233fae2fae674746226115dffcb1e2
Giridharan <151344363+MGDharan@users.noreply.github.com>
2025-12-29 19:55:51 +0530

    Create Class.java

---
+++ 28.12.25/Class.java
@@ -0,0 +1,10 @@
+import java.util.Optional;
+
+public class OptionalDemo {
+    public static void main(String[] args) {
+        String name = null;
+
+        Optional<String> opt = Optional.ofNullable(name);
+        System.out.println(opt.orElse("Default Name"));
+    }
+}
