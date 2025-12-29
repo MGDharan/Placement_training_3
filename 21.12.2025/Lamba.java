b284487cf1ff64043888dacc7255f926b0ff665b
Giridharan <151344363+MGDharan@users.noreply.github.com>
2025-12-29 17:22:01 +0530

    Add Calculator interface and LambdaDemo class

---
+++ 21.12.2025/Lamba.java
@@ -0,0 +1,10 @@
+interface Calculator {
+    int add(int a, int b);
+}
+
+public class LambdaDemo {
+    public static void main(String[] args) {
+        Calculator c = (a, b) -> a + b;
+        System.out.println(c.add(10, 20));
+    }
+}
