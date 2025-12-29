cbdf3a79b2da9b8df1eae70909263084d2327849
Giridharan <151344363+MGDharan@users.noreply.github.com>
2025-12-29 19:50:28 +0530

    Add NeonNumber class to check for Neon numbers

---
+++ 27.12.25/Neon.java
@@ -0,0 +1,12 @@
+public class NeonNumber {
+    public static void main(String[] args) {
+        int n = 9;
+        int sq = n * n, sum = 0;
+
+        while (sq > 0) {
+            sum += sq % 10;
+            sq /= 10;
+        }
+        System.out.println(sum == n ? "Neon Number" : "Not Neon");
+    }
+}
