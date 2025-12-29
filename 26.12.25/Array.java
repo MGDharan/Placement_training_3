df69886c536557c05faadf883d208b64e1da34fe
Giridharan <151344363+MGDharan@users.noreply.github.com>
2025-12-29 19:44:57 +0530

    Add EvenOddCount class to count even and odd numbers

---
+++ 26.12.25/Array.java
@@ -0,0 +1,13 @@
+public class EvenOddCount {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int even = 0, odd = 0;
+
+        for (int i : arr) {
+            if (i % 2 == 0) even++;
+            else odd++;
+        }
+        System.out.println("Even: " + even);
+        System.out.println("Odd: " + odd);
+    }
+}
