732ae4ac1407a51b90a7b996086ec32d320908b3
Giridharan <151344363+MGDharan@users.noreply.github.com>
2025-12-29 19:47:11 +0530

    Add StringLength class to count characters in a string

---
+++ 26.12.25/String.java
@@ -0,0 +1,11 @@
+public class StringLength {
+    public static void main(String[] args) {
+        String s = "Java";
+        int count = 0;
+
+        for (char c : s.toCharArray())
+            count++;
+
+        System.out.println(count);
+    }
+}
