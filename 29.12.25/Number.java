b99a8f7f36c216016f15819ccf89ffb227cd07ec
Giridharan <151344363+MGDharan@users.noreply.github.com>
2025-12-29 20:00:06 +0530

    Create Number.java

---
+++ 29.12.25/Number.java
@@ -0,0 +1,11 @@
+public class DigitCount {
+    public static void main(String[] args) {
+        int n = 12345, count = 0;
+
+        while (n > 0) {
+            count++;
+            n /= 10;
+        }
+        System.out.println(count);
+    }
+}
