db9e3b35534c97f0dfdf9d466fcd68ad239d5c52
Giridharan <151344363+MGDharan@users.noreply.github.com>
2025-12-29 17:28:21 +0530

    Add MathOperation class with overloaded add methods

---
+++ 22.12.2025/Method.java
@@ -0,0 +1,16 @@
+class MathOperation {
+    int add(int a, int b) {
+        return a + b;
+    }
+    double add(double a, double b) {
+        return a + b;
+    }
+}
+
+public class Overload {
+    public static void main(String[] args) {
+        MathOperation m = new MathOperation();
+        System.out.println(m.add(10, 20));
+        System.out.println(m.add(10.5, 20.5));
+    }
+}
