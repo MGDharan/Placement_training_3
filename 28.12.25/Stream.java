463174ccb14d60a2cd41c1d87dfed469aac3ffaf
Giridharan <151344363+MGDharan@users.noreply.github.com>
2025-12-29 19:58:56 +0530

    Add StreamSum class to calculate array sum

---
+++ 28.12.25/Stream.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class StreamSum {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4};
+        int sum = Arrays.stream(arr).sum();
+        System.out.println(sum);
+    }
+}
